ad9336a3cc51f6398f4a61d66a9a56816baf5f89
Augustin-Erceville <a.erceville@lprs.fr>
2025-05-02 09:10:51 +0200

    V1.8 - Ajout du bouton de suppression et ajout de la fonctionnalité de suppression d'un utilisateur

--- src/main/java/appli/acceuil/TableController.java
+++ src/main/java/appli/acceuil/TableController.java
@@ -4,11 +4,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import model.Utilisateur;
 import repository.UtilisateurRepository;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.List;
@@ -19,26 +19,52 @@ public class TableController implements Initializable {
     @FXML
     private TableView<Utilisateur> tableau;
 
+    @FXML
+    private Button supprimerBtn;
+
+    private Utilisateur utilisateurSelectionne;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String[][] colonnes = {
-                {"Id Utilisateur", "id_utilisateur"},
-                {"Nom", "nom"},
-                {"Prénom", "prenom"},
-                {"Email", "email"},
-                {"Rôle", "role"},
-        };
+        TableColumn<Utilisateur, Integer> colId = new TableColumn<>("Identifiant");
+        colId.setCellValueFactory(new PropertyValueFactory<>("id_utilisateur"));
 
-        for (String[] col : colonnes) {
-            TableColumn<Utilisateur, String> maCol = new TableColumn<>(col[0]);
-            maCol.setCellValueFactory(new PropertyValueFactory<>(col[1]));
-            tableau.getColumns().add(maCol);
+        TableColumn<Utilisateur, String> colNom = new TableColumn<>("Nom");
+        colNom.setCellValueFactory(new PropertyValueFactory<>("nom"));
 
-            UtilisateurRepository utilisateurRepository = new UtilisateurRepository();
+        TableColumn<Utilisateur, String> colPrenom = new TableColumn<>("Prénom");
+        colPrenom.setCellValueFactory(new PropertyValueFactory<>("prenom"));
 
-            List<Utilisateur> utilisateurs = utilisateurRepository.getTousLesUtilisateurs();
-            ObservableList<Utilisateur> data = FXCollections.observableArrayList(utilisateurs);
-            tableau.setItems(data);
+        TableColumn<Utilisateur, String> colEmail = new TableColumn<>("Email");
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+
+        TableColumn<Utilisateur, String> colRole = new TableColumn<>("Rôle");
+        colRole.setCellValueFactory(new PropertyValueFactory<>("role"));
+
+        tableau.getColumns().addAll(colId, colNom, colPrenom, colEmail, colRole);
+
+        UtilisateurRepository utilisateurRepository = new UtilisateurRepository();
+        List<Utilisateur> utilisateurs = utilisateurRepository.getTousLesUtilisateurs();
+        ObservableList<Utilisateur> data = FXCollections.observableArrayList(utilisateurs);
+        tableau.setItems(data);
+
+        supprimerBtn.setDisable(true);
+    }
+
+    @FXML
+    private void cliqueTableauEvent(MouseEvent event) {
+        utilisateurSelectionne = tableau.getSelectionModel().getSelectedItem();
+        supprimerBtn.setDisable(utilisateurSelectionne == null);
+    }
+
+    @FXML
+    private void supprimerUtilisateur() {
+        if (utilisateurSelectionne != null) {
+            UtilisateurRepository repo = new UtilisateurRepository();
+            repo.supprimerUtilisateurParEmail(utilisateurSelectionne.getEmail());
+            tableau.getItems().remove(utilisateurSelectionne);
+            utilisateurSelectionne = null;
+            supprimerBtn.setDisable(true);
         }
     }
 }
